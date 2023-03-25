54123bffaa089284040fa865af11bbfe933a3706
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-03-25 23:15:19 +0530

    Update ecs_main.tf

--- ecs_main.tf
+++ ecs_main.tf
@@ -3,8 +3,82 @@ provider "aws" {
   region  = "us-east-1" 
 }
 
+#resource "aws_ecr_repository" "my_first_ecr_repo" {
+# name = "my-first-ecr-repo" 
+#}
 resource "aws_ecr_repository" "my_first_ecr_repo" {
-  name = "my-first-ecr-repo" 
+  name                 = "docker_ecr_repo"
+  image_tag_mutability = var.immutable_ecr_repositories ? "IMMUTABLE" : "MUTABLE"
+  force_delete         = true
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+  tags = {
+    Name  = "worker-repository"
+    Group = "test"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+  repository = aws_ecr_repository.ecr_repo.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository_policy" "demo-repo-policy" {
+  repository = aws_ecr_repository.ecr_repo.name
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "Set the permission for ECR",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
+resource "null_resource" "update_docker_fund" {
+  provisioner "local-exec" {
+    working_dir = "nginx"
+    command     = "chmod +x update-ecr.sh && sh -x update-ecr.sh"
+  }
+
+  depends_on = [aws_ecr_repository.ecr_repo, aws_ecr_lifecycle_policy.ecr_policy, aws_ecr_repository_policy.demo-repo-policy]
 }
 
 
